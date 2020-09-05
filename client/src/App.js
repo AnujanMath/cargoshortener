8c7dd6fe80146871175457a3c1697cf3e9132954
Anujan <anujanftw@gmail.com>
2020-09-04 21:13:42 -0400

    Added a lot of UI changes to app.js, alerts and validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import SendIcon from "@material-ui/icons/Send";
+import Alert from "@material-ui/lab/Alert";
+import Collapse from "@material-ui/core/Collapse";
 
 class App extends React.Component {
   constructor(props) {
@@ -21,14 +26,19 @@ class App extends React.Component {
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "", button: "" };
-
+    this.state = {
+      value: "",
+      button: false,
+      error: false,
+      collapse: false,
+      url: "",
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value, button: true });
+    this.setState({ button: false });
   }
 
   handleSubmit(event) {
@@ -46,33 +56,51 @@ class NameForm extends React.Component {
     })
       .then((response) => {
         if (response.status === 422) {
-          alert("Invalid URL!");
+          this.setState({ error: true });
         } else {
           response.json().then((data) => {
-            alert("Your url is now available at: " + data.shortUrl);
+            this.setState({ error: false, url: data.shortUrl });
           });
         }
       })
       .catch(function (res) {
         console.log(res);
       });
-    this.setState({ button: false });
+    this.setState({ button: true, collapse: true });
 
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" disabled={!this.state.button} />
+      <form onSubmit={this.handleSubmit} noValidate autoComplete="off">
+        <TextField
+          error={this.state.error}
+          id="outlined-error-helper-text"
+          label="Enter URL"
+          helperText={this.state.error ? "Invalid URL!" : ""}
+          variant="outlined"
+          onChange={this.handleChange}
+        />
+        <Button
+          variant="contained"
+          disabled={this.state.button}
+          color="primary"
+          endIcon={<SendIcon></SendIcon>}
+        >
+          Send
+        </Button>
+        <Collapse in={this.state.collapse}>
+          <Alert
+            onClose={() => {
+              this.setState({ collapse: false });
+            }}
+            variant="filled"
+            severity="success"
+          >
+            Your URL is now available at {this.state.url}
+          </Alert>
+        </Collapse>
       </form>
     );
   }
